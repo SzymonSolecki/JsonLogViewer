53e2f0d6053ab06456aba291ca1e21a1697ffe3f
Szymon <szymon@fedora.home>
2025-09-27 19:42:01 +0200

    setup main

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"os"
 
 	"github.com/SzymonSolecki/json-log-viewer/pkg/data"
-	"github.com/SzymonSolecki/json-log-viewer/pkg/ui"
+	"github.com/SzymonSolecki/json-log-viewer/pkg/ui/pages"
+	"github.com/rivo/tview"
 )
 
 func main() {
@@ -17,23 +18,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	headers, parsedData, err := data.ParseJSON(os.Stdin)
+	parsedData, err := data.ParseJSON(os.Stdin)
 	if err != nil {
 		fmt.Printf("Error parsing JSON: %v\n", err)
 		os.Exit(1)
 	}
-	if len(headers) == 0 {
+	if len(parsedData.Headers) == 0 {
 		fmt.Println("No headers found")
 		os.Exit(0)
 	}
-	if len(parsedData) == 0 {
+	if len(parsedData.Data) == 0 {
 		fmt.Println("No data found")
 		os.Exit(0)
 	}
 
-	app := ui.NewApp()
+	app := tview.NewApplication()
+	pages := pages.NewRootPage(parsedData, app)
 
-	if err := app.Run(headers, parsedData); err != nil {
+	app.SetRoot(pages, true)
+
+	if err := app.Run(); err != nil {
 		panic(err)
 	}
 }
